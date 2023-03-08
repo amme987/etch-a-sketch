666c47a54cab6ef748b3d2076a6b242a9d0b2927
amme987 <emmagination97@gmail.com>
2023-03-08 09:00:04 -0500

    Change grid colors when mouse hovers over them

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+// console.log(divs[0].textContent);
 let i = 1;
 while (i <= 256) {
   const div = document.createElement("div");
@@ -8,5 +9,15 @@ while (i <= 256) {
   i++;
 }
 
+const divs = Array.from(document.querySelectorAll(".container div"));
+
 container.style.cssText =
   "grid-template-rows: repeat(16, auto); grid-template-columns: repeat(16, auto);";
+
+divs.forEach((div) =>
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "aqua";
+  })
+);
+
+// container.addEventListener('mouseover', e => console.log(e));
