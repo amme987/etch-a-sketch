9bdf99ef65133d1a127ca4fa5162953e237d7c27
amme987 <emmagination97@gmail.com>
2023-03-08 07:34:04 -0500

    Create 16x16 grid of square divs

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 const container = document.querySelector(".container");
 let i = 1;
-while(i <= 16){
-    const div = document.createElement("div");
-    div.textContent = i;
-    container.appendChild(div);
-    i++;
+while (i <= 256) {
+  const div = document.createElement("div");
+  div.textContent = i;
+  container.appendChild(div);
+  // container.style.cssText = 'grid-template-rows: auto; grid-template-columns: auto;';
+  i++;
 }
 
+container.style.cssText =
+  "grid-template-rows: repeat(16, auto); grid-template-columns: repeat(16, auto);";
