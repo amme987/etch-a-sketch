1a450509192656839837db5aaf21b0a9c37fd586
amme987 <emmagination97@gmail.com>
2023-03-08 11:12:45 -0500

    Change number of squares for grid based on user input

--- javascript.js
+++ javascript.js
@@ -1,23 +1,55 @@
 const container = document.querySelector(".container");
-// console.log(divs[0].textContent);
-let i = 1;
-while (i <= 256) {
-  const div = document.createElement("div");
-  div.textContent = i;
-  container.appendChild(div);
-  // container.style.cssText = 'grid-template-rows: auto; grid-template-columns: auto;';
-  i++;
+const button = document.querySelector("button");
+let divs;
+let number = 16;
+
+button.addEventListener("click", () => {
+  do {
+    number = Number(
+      prompt("Enter number of squares per side for new grid", 16)
+    );
+  } while (number > 100);
+
+  //   console.log(number);
+  deleteGrid(divs);
+  createGrid(number);
+
+  //   return number;
+});
+
+// Generate grid with specified number of rows and columns
+function createGrid() {
+  let i = 1;
+  let max = Math.pow(number, 2);
+  while (i <= max) {
+    const div = document.createElement("div");
+    div.textContent = i;
+    container.appendChild(div);
+    i++;
+  }
+  container.style.cssText = `grid-template-rows: repeat(${number}, auto); grid-template-columns: repeat(${number}, auto);`;
+  divs = Array.from(document.querySelectorAll(".container div"));
+
+  // Change color of divs
+  divs.forEach((div) =>
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "aqua";
+    })
+  );
 }
 
-const divs = Array.from(document.querySelectorAll(".container div"));
+function deleteGrid(divs) {
+  divs.forEach((div) => {
+    container.removeChild(div);
+  });
+}
 
-container.style.cssText =
-  "grid-template-rows: repeat(16, auto); grid-template-columns: repeat(16, auto);";
+createGrid();
 
-divs.forEach((div) =>
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "aqua";
-  })
-);
-
-// container.addEventListener('mouseover', e => console.log(e));
+// function changeColor(divs) {
+//   divs.forEach((div) =>
+//     div.addEventListener("mouseover", () => {
+//       div.style.backgroundColor = "aqua";
+//     })
+//   );
+// }
